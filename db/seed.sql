79726f3515f8f6009475233214ff14b1c1b7248b
Cathy Richardson <cathyanne08@gmail.com>
2021-10-26 22:06:57 -0600

    Updated db product table, and showed it on page

--- db/seed.sql
+++ db/seed.sql
@@ -8,9 +8,11 @@ CREATE TABLE beauty_user (
 
 CREATE TABLE skincare_product (
     id SERIAL PRIMARY KEY,
+    image TEXT NOT NULL,
     name VARCHAR(150) NOT NULL,
     type  VARCHAR(150) NOT NULL,
     price FLOAT,
+    size VARCHAR(50),
     review TEXT,
     is_recommended BOOLEAN
 ); 
@@ -26,8 +28,20 @@ CREATE TABLE skincare_comment (
 INSERT INTO beauty_user(name, email, password, is_admin)
 VALUES ('Test', 'test@test.com', 'test-password', false);
 
-INSERT INTO skincare_product(name, type, price, review, is_recommended)
-VALUES ('Test-product', 'test-type', 45.01, 'test-review', false);
+
+
+INSERT INTO skincare_product(image, name, type, price, size, review, is_recommended)
+VALUES 
+('https://www.dhccare.com/media/catalog/product/cache/02cccf94ae90183c4f9b773c23a87fc4/d/h/dhc_deep_cleansing_oil_h_300.png', 
+'DHC Deep Cleansing Oil', '1st Cleanse/ Makeup Remover', 24.00, '6.7 fl. oz.', 'A good basic cleansing oil', true),
+
+('https://www.vanicream.com/dynamic-media/product/images/vanicream-facial-cleanser-8oz-front-bp20a.jpg?gravity=center&v=galleryZoom&k=6kRgk91BEufhjMU%2BV6bf6A',
+'Vanicream Gentle Facial Cleanser', '2nd Cleanse/ AM Face Wash', 8.99, '8 fl. oz.','A good basic cleanser', true),
+
+('https://cdn.shopify.com/s/files/1/1525/1400/products/0365_front_540x.jpg?v=1569333261',
+'DERMA E Vitamin C Serum', 'Vitamin C Serum', 19.99, '2 fl. oz.','A good basic vitamin C serum', true);
+
+
 
 INSERT INTO skincare_comment(user_id, product_id, review, is_recommended)
 VALUES (1, 1, 'user-review', true);
--- src/Components/SkincareProductsList.js
+++ src/Components/SkincareProductsList.js
@@ -4,35 +4,30 @@ import axios from 'axios'
 
 function SkincareProductsList() {
 
-  // const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([])
 
-  // useEffect(async () => {
-  //   try {
-  //     // try getting all the products to show frontend is connecting to backend
-  //     const { data } = await axios.get(`/api/skincare/products`);
-  //     setProducts(data);
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }, [])
+    useEffect(async () => {
+        try {
+            // try getting all the products to show frontend is connecting to backend
+            const { data } = await axios.get(`/api/skincare/products`);
+            setProducts(data);
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
 
-  return (
+    return (
 
-    <div className="SkincareProduct">
-        <h1>Skincare Products List</h1>
-    </div>
-
-
-    // <div className="App">
-    //   {/* just display the products list for now  */}
-    //   <h1> Products List </h1>
-    //   {products.map((item, index) => {
-    //     return <p key={index}>{item.name}</p>
-    //   })}
-    // </div>
+        <div className="SkincareProduct">
+            <h1>Skincare Products List</h1>
+            {/* just display the products list for now  */}
+            {products.map((item, index) => {
+                return <p key={index}>{item.name}, ${item.price} {item.size}</p>
+            })}
+        </div>
 
 
-  );
+    );
 }
 
 export default SkincareProductsList;
