740b3682585ec4c6f83181be1beddeb228074b56
Cathy Richardson <cathyanne08@gmail.com>
2021-11-16 18:52:34 -0700

    adding css to homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,30 @@
-import '../App.scss';
+import './Home.scss';
+import makeupSquare from '../images/makeup.jpg'
 
 function Home() {
- 
+
   return (
     <div className="home">
-        <h1>Home</h1>
+      <main>
+        <h1>Best in Beauty</h1>
+        <p>Find the top skincare curated to your personal taste!</p>
+        <button>Start Now</button>
+      </main>
+      <h2>Read reviews before you buy</h2>
+      <section className="three-links">
+        <img className="homepage-square"
+          src={makeupSquare}
+          alt="makeup items flatlay" />
+        <h4>Skincare</h4>
+        <img className="homepage-square"
+          src={makeupSquare}
+          alt="makeup items flatlay" />
+        <h4>Makeup</h4>
+        <img className="homepage-square"
+          src={makeupSquare}
+          alt="makeup items flatlay" />
+        <h4>Haircare</h4>
+      </section>
     </div>
   );
 }
